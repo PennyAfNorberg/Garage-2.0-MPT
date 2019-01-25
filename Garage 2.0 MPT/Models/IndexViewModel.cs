29b1366e405beac0c5ba71a767d66366c2632fee
Penny <pennyafnorberg@hotmail.com>
2019-01-25 15:35:49 +0100

    Ändrat visade timmar och påbörjade timmar. lagt till viewmodel för index.

---
+++ Garage 2.0 MPT/Models/IndexViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0_MPT.Models
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Type")]
+        public VehicleTyp VehicleTyp { get; set; }
+        [Display(Name = "Reg Nr")]
+        public string RegNr { get; set; }
+        [Display(Name = "Color")]
+        public string VehicleColor { get; set; }
+        [Display(Name = "Model")]
+        public string VehicleModel { get; set; }
+        [Display(Name = "Brand")]
+        public string VehicleBrand { get; set; }
+        [Display(Name = "Wheels")]
+        public int NumberOfWheels { get; set; }
+        [Display(Name = "Parked Time")]
+        public string ParkedTime { get; set; }
+        public int ParkedHours { get; set; }
+    }
+}
