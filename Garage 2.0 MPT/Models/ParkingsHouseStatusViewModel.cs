c8c905a46f7b46fd8ed3cb8569671ea845014524
PennyAfNorberg <37243833+PennyAfNorberg@users.noreply.github.com>
2019-01-30 20:04:42 +0100

    building some statusreports

---
+++ Garage 2.0 MPT/Models/ParkingsHouseStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Garage_2._0_MPT.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0_MPT.Models
+{
+    public class ParkingsHouseStatusViewModel
+    {
+        public Dictionary<string,Position> NextFree { get; set; }    
+
+    }
+}
