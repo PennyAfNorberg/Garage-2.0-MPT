52d586387d86e214b4b3cf468cc488558b5f8848
Penny <pennyafnorberg@hotmail.com>
2019-02-06 11:45:28 +0100

    men varför skicka upp sql?

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+﻿ select *
+  from Members t1
+
+  select *
+   from Vehicles t1
+   left join ParkedVehicle t2 on t2.VehicleId=t1.Id
+
+   select *
+    from VehicleTyp
+
+	select *
+	 from ParkedVehicle t1
